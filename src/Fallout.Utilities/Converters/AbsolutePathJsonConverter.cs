11c10ce43324ecb7a3587581f9b9a4d9f6beda1e
Lukas Gasselsberger | alu-one <l.gasselsberger@alu-one.at>
2026-07-29 09:08:58 +0200

    Add an additional type converter for `System.Text.Json`
    (cherry picked from commit c832ec3c492ae2e29b497fc86d8f319c1e220d08)

---
+++ src/Fallout.Utilities/Converters/AbsolutePathJsonConverter.cs
@@ -0,0 +1,35 @@
+#if NET6_0_OR_GREATER
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Fallout.Common;
+using Fallout.Common.IO;
+using static Fallout.Common.IO.PathConstruction;
+
+namespace Fallout.Utilities.Converters;
+
+public sealed class AbsolutePathJsonConverter : JsonConverter<AbsolutePath>
+{
+    public override AbsolutePath Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        if (value != null)
+            return HasPathRoot(value)
+                ? AbsolutePath.Create(value)
+                : EnvironmentInfo.WorkingDirectory / value;
+
+        return null;
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        AbsolutePath value,
+        JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value);
+    }
+}
+#endif
