14ffaa9dbfa076b05fa2eb976a168bdc91ee76be
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2026-08-24 07:44:20 +0200

    Regenerate tool wrappers with develop as the new source-link branch

--- src/Fallout.Common/Tools/CodeMetrics/CodeMetrics.Generated.cs
+++ src/Fallout.Common/Tools/CodeMetrics/CodeMetrics.Generated.cs
@@ -1,4 +1,4 @@
-// Generated from https://github.com/Fallout-build/Fallout/blob/main/src/Fallout.Common/Tools/CodeMetrics/CodeMetrics.json
+// Generated from https://github.com/Fallout-build/Fallout/blob/develop/src/Fallout.Common/Tools/CodeMetrics/CodeMetrics.json
 
 using Fallout.Common;
 using Fallout.Common.Tooling;
--- src/Fallout.Common/Tools/MSpec/MSpec.Generated.cs
+++ src/Fallout.Common/Tools/MSpec/MSpec.Generated.cs
@@ -1,4 +1,4 @@
-// Generated from https://github.com/Fallout-build/Fallout/blob/main/src/Fallout.Common/Tools/MSpec/MSpec.json
+// Generated from https://github.com/Fallout-build/Fallout/blob/develop/src/Fallout.Common/Tools/MSpec/MSpec.json
 
 using Fallout.Common;
 using Fallout.Common.Tooling;
